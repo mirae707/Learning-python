4471cbfd79b6c1d9e751c5a43e7bf4728454b3c0
Jun <fohens295@gmail.com>
2020-11-30 19:31:54 +0900

    static project

--- Django/first/first_app/views.py
+++ Django/first/first_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<em>My third project</em>")
+
+def pic(request):
+    pic_dict = {'pic_insert': 'Skelleton'}
+    return render(request, 'first_app/pic.html', context=pic_dict)
