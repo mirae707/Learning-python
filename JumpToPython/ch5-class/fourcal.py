da10b93f997f5dc57e7ea64f2790f931f672a133
Jun <fohens295@gmail.com>
2020-12-16 17:32:16 +0900

    Finished chapter 5

--- JumpToPython/ch5-class/fourcal.py
+++ JumpToPython/ch5-class/fourcal.py
@@ -1,8 +1,46 @@
 class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
     def setdata(self, first, second):
         self.first = first
         self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
 
-a = FourCal()
-print(type(a))
-print(a.setdata(4, 2))
+a = FourCal(4, 0)
+b = FourCal(5, 2)
+c = FourCal(6, 7)
+d = FourCal(9, 3)
+
+print(a.add())
+print(b.mul())
+print(c.sub())
+print(d.div())
+
+# class 상속
+class MoreFourCal(FourCal):
+    def pow(self):
+        result = self.first ** self.second
+        return result
+
+a = MoreFourCal(4, 2)
+print(a.pow())
+
+# method overriding
+class SafeFourCal(FourCal):
+    def div(self):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first / self.second
