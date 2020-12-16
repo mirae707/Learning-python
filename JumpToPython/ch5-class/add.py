f78bfa1b275423099994c6ba96f1a46af3fe4757
Jun <fohens295@gmail.com>
2020-12-16 15:22:20 +0900

    update

--- JumpToPython/ch5-class/add.py
+++ JumpToPython/ch5-class/add.py
@@ -1,9 +1,15 @@
-result = 0
+class Calculator:
+    def __init__(self):
+        self.result = 0
 
-def add(num):
-    global result
-    result += num
-    return result
+    def add(self, num):
+        self.result += num
+        return self.result
 
-print(add(3))
-print(add(4))
+cal1 = Calculator()
+cal2 = Calculator()
+
+print(cal1.add(3))
+print(cal1.add(4))
+print(cal2.add(3))
+print(cal2.add(7))
---
+++ JumpToPython/ch5-class/fourcal.py
@@ -0,0 +1,8 @@
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+
+a = FourCal()
+print(type(a))
+print(a.setdata(4, 2))
