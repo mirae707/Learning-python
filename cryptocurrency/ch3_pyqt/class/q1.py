aba671ec9e81ffd7605986f503015f6055933df2
jun <chchdelm3@icloud.com>
2021-02-17 12:01:49 +0900

    started pyqt

---
+++ cryptocurrency/ch3_pyqt/class/q1.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, name, author, publisher, isb10):
+        self.name = name
+        self.author = author
+        self.publisher = publisher
+        self.isb10 = isb10
