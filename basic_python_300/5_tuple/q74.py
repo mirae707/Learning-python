d899922afb03833f5b8ed127828f26772dd1ed2d
jun <chchdelm3@icloud.com>
2021-02-17 12:35:06 +0900

    finished tuple

---
+++ basic_python_300/5_tuple/q74.py
@@ -0,0 +1,2 @@
+t = (1, 2, 3)
+t[0] = 'a' # The tuple can not change element
---
+++ basic_python_300/5_tuple/q80.py
@@ -0,0 +1,10 @@
+"""
+even = []
+for x in range(2, 100, 2):
+    even.append(x)
+
+even = tuple(even)
+print(even)
+"""
+data = tuple(range(2, 100, 2))
+print(data)
