10daa7ee2b3ee364fbaabfafea4b14483e05f1a8
jun <chchdelm3@icloud.com>
2021-03-23 11:46:09 +0900

    trading scripts

---
+++ trading.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+source /home/xnote/venvs/crypto/bin/activate
+
+/usr/bin/python3 /home/xnote/bot_bithumb/ada/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/amo/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/anw/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/bch/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/bcha/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/btc/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/btt/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/burger/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/eos/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/etc/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/eth/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/ltc/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/luna/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/mlk/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/mm/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/npxs/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/qtum/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/sand/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/steem/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/sushi/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/temco/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/trx/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/xlm/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/xno/main.py &
+/usr/bin/python3 /home/xnote/bot_bithumb/xrp/main.py &
