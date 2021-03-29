1de7dbdadc1752aa80caf98f2180c996b2ada4ed
jun <chchdelm3@icloud.com>
2021-03-29 18:43:25 +0900

    added a scalping strategy

---
+++ Crypto/upbit_scalping/macd.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+#import time
+
+def cal_macd(ticker):
+    url = "https://api.upbit.com/v1/candles/minutes/5"
+
+    querystring = {"market":ticker,"count":"100"}
+    response = requests.request("GET", url, params=querystring)
+    data = response.json()
+    df = pd.DataFrame(data)
+    df=df.iloc[::-1]
+    df=df['trade_price']
+
+    exp1 = df.ewm(span=12, adjust=False).mean()
+    exp2 = df.ewm(span=26, adjust=False).mean()
+    macd = exp1-exp2
+    exp3 = macd.ewm(span=9, adjust=False).mean()
+
+#    print('MACD: ',macd[0])
+#    print('Signal: ',exp3[0])
+
+    test1=exp3[0]-macd[0]
+    test2=exp3[1]-macd[1]
+    call='no'
+
+    if test1<0 and test2>0:
+       call='sell'
+    if test1>0 and test2<0:
+       call='buy'
+    return call
---
+++ Crypto/upbit_scalping/main.py
@@ -0,0 +1,64 @@
+from bollinger import *
+from macd import *
+from rsi import *
+#from stoch_rsi import *
+import pyupbit
+import numpy as np
+import time
+
+ticker = "KRW-TRX"
+
+# 객체 생성
+f = open("upbit.txt")
+lines = f.readlines()
+access = lines[0].strip()
+secret = lines[1].strip()
+f.close()
+upbit = pyupbit.Upbit(access, secret)
+
+start_balance = upbit.get_balance("KRW")
+end_balance = upbit.get_balance("KRW")
+
+op_mode = False # False 작동안함 True 작동함
+hold = False # 코인 보유 여부
+
+while True:
+    df = pyupbit.get_ohlcv(ticker, interval="minute5", count=100) # 5분봉 데이터 가져오기
+
+    price = pyupbit.get_current_price(ticker) # 현재 코인 가격
+    target_volume = np.mean(df.iloc[90:100]['volume']) * 1.5 # 목표 거래량
+    current_volume = df.iloc[-1]['volume'] # 현재 거래량
+
+    bband_width = bb(ticker)
+    rsi =  cal_rsi(ticker)
+    macd = cal_macd(ticker)
+
+    try:
+        if current_volume > target_volume and bband_width < 0.02 and rsi < 35 and macd == 'buy' and op_mode == True and hold == False:
+            limit = price * 0.09 # 손절 가격
+            profit = price * 1.01 # 익절 가격
+            my_balance = upbit.get_balance("KRW") - 1000 # 원화 잔고
+            upbit.buy_market_order(ticker, my_balance)
+            time.sleep(5)
+            hold = True
+            print(ticker, "를 매수했습니다.\n 매도할 타이밍을 지켜보는 중입니다....")
+
+        if hold == True and price < limit:
+            coin_balance = upbit.get_balance(ticker)  # 코인 잔고
+            upbit.sell_market_order(ticker, coin_balance) # 코인 전량 매도
+            time.sleep(5)
+            end_balance = upbit.get_balance("KRW")
+            rate_of_profit = (start_balance - end_balance) / start_balance
+            hold = False
+            print(f"손절했습니다. 죄송합니다.\n시작금액: {start_balance} 현재잔고: {end_balance} 수익률: {rate_of_profit}")
+
+        if hold == True and price > profit:
+            coin_balance = upbit.get_balance(ticker)  # 코인 잔고
+            upbit.sell_market_order(ticker, coin_balance) # 코인 전량 매도
+            time.sleep(5)
+            end_balance = upbit.get_balance("KRW")
+            rate_of_profit = (start_balance - end_balance) / start_balance
+            hold = False
+            print(f"이익봤어요! 축하드려요~\n시작금액: {start_balance} 현재잔고: {end_balance} 수익률: {rate_of_profit}")
+    except:
+        pass
